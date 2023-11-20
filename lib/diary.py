e5d6aba134d784592d3030e291e0887ec47da592
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2023-11-20 10:41:03 +0000

    finished diary

--- lib/diary.py
+++ lib/diary.py
@@ -1,7 +1,10 @@
 class Diary:
     def __init__(self, contents):
         # contents is a string
-        self.diary_contents = contents
+        if contents == None or len(contents) <= 0:
+            raise Exception("Invalid entry, 'contents' must have at least one character.")
+        else:
+            self.diary_contents = contents
 
     def read(self):
         # Returns the contents of the diary
--- tests/test_diary.py
+++ tests/test_diary.py
@@ -9,3 +9,14 @@ def test_if_reads_diary():
     diary = Diary('15/11/2023 Gym: Completed upper body day 1 weightlifting program today')
     result = diary.read()
     assert result == '15/11/2023 Gym: Completed upper body day 1 weightlifting program today'
+
+
+"""
+Given an instance of the class without contents
+#read throws an error
+"""
+def test_if_throws_error_when_empty_argument():
+    with pytest.raises(Exception) as e: 
+        Diary('')
+    error_message = str(e.value)
+    assert error_message == "Invalid entry, 'contents' must have at least one character."
